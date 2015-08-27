ca9525350f7c1a6b681f4fb06b7f5fe2be2f1396
Andrew McKnight <mcknightaa@gmail.com>
2015-08-27 13:23:50 -0400

    initial commit for careify display site

---
+++ packages/core/system/public/assets/static/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// reCaptcha Setup
+// ===============
+
+// Insert below your reCaptcha Public and Private Keys
+// Go to https://www.google.com/recaptcha/admin/create if you don't have the keys yet
+
+$publickey = "6Lc-5AsTAAAAACXMjsJBFOkiXuOby2OJKr5lfxUM";
+$privatekey = "6Lc-5AsTAAAAAGTs_7PmnQnPMcp9_hHGK9qRT5Bu";
+
+// Mail Setup
+// ==========
+
+// Sender Name and <email address> separated by space
+$mail_sender = 'Makana <makana@careify.io>';
+// Your Email Address where new emails will be sent to
+$to_email = 'mdumlao@mac.com';
+// Email Subject
+$mail_subject = 'Subscribed to Careify';
+
+// Email content can be modified in the sendmail.php file.
+
+?>
